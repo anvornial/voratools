efdc0f1563147f938f8459cdc9a2dad33dcaf634
Voranial Mewmew <anvornial@gmail.com>
2018-10-19 07:36:05 +0700

    Correcting repeat and adding nvmwrapper

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Feel free to use it as you wish with your own risk. All Licensed with WTFPL unle
 
 * waiter  
     python blocking script that connected to dbus, waiting and direct each call message to stdio. It's mainly used to provide bash script with basic dbus capability
+
+* nvmwrapper
+    wrapper script to install, upgrade and load nvm on demand. This script is created because loading nvm every 
+.bashrc invocation is slow.
---
+++ nvmwrapper.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Simple wrapper script to install, load, and upgrade nvm
+# please source this script with $NVM_DIR of your choice
+
+# set NVM_DIR if not exist
+if [[ -z ${NVM_DIR+x} ]]; then
+  export NVM_DIR="$HOME/.nvm"
+fi
+
+# function to load nvm
+function nvmload {
+  if [[ -d $NVM_DIR ]]; then
+    export NPM_CONFIG_USERCONFIG="$NVM_DIR/npmrc_user"
+    export NPM_CONFIG_GLOBALCONFIG="$NVM_DIR/npmrc_global"
+
+    # loads nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
+
+    # loads nvm bash_completion
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+
+    return 0
+  else
+    echo "Error, $NVM_DIR is NOT exist, install NVM first."
+    return 1
+  fi
+  echo "Error loading NVM"
+  return 1
+}
+
+
+# function to install NVM into $NVM_DIR
+function nvminstall {
+  if [[ ! -d $NVM_DIR ]]; then
+    git clone https://github.com/creationix/nvm.git "$NVM_DIR" &&
+    cd "$NVM_DIR" &&
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` &&
+
+    nvmload &&
+
+    nvm install node &&
+    npm config set cache "$NVM_DIR/npm_cache_user" &&
+
+    return 0
+  else
+    echo "Error, $NVM_DIR is already exist, delete first before installation"
+    return 1
+  fi
+  echo "Error installing NVM"
+  return 1
+}
+
+
+# function upgrade nvm
+function nvmupgrade {
+  if [[ -d $NVM_DIR ]]; then
+    cd "$NVM_DIR"
+    git fetch --tags origin &&
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` &&
+    \. "$NVM_DIR/nvm.sh"
+    return 0
+  else
+    echo "Error, $NVM_DIR is NOT exist, install NVM first."
+    return 1
+  fi
+  echo "Error upgrading NVM"
+  return 1
+}
