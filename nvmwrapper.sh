59a1136da5bfc192eb717cb830b3356103e68302
Voranial Mewmew <anvornial@gmail.com>
2020-05-28 20:24:45 +0700

    Update for waiter, fix for nvmwrapper

--- nvmwrapper.sh
+++ nvmwrapper.sh
@@ -11,12 +11,10 @@ fi
 # function to load nvm
 function nvmload {
   if [[ -d $NVM_DIR ]]; then
-    
     if [[ $NPM_CONFIG_PREFIX != "" ]]; then
       echo "NPM_CONFIG_PREFIX Environment variable is not empty"
-      echo "It will be unset and copied to _NPM_CONFIG_PREFIX"
-      NPM_CONFIG_PREFIX_OLD=$_NPM_CONFIG_PREFIX
-      export _NPM_CONFIG_PREFIX
+      echo "It will be unset and copied to _NPMCONFIGPREFOLD"
+      export _NPMCONFIGPREFOLD="$NPM_CONFIG_PREFIX"
     fi
     
     unset NPM_CONFIG_PREFIX
@@ -31,22 +29,32 @@ function nvmload {
 
     return 0
   else
-    echo "Error, $NVM_DIR is NOT exist, install NVM first."
+    echo "Error, $NVM_DIR is NOT exist, install nvm first."
     return 1
   fi
-  echo "Error loading NVM"
+  echo "Error loading nvm"
   return 1
 }
 
 function nvmunload {
+  if [[ -z $(command -v nvm)  ]]; then
+    echo "nvm is not loaded"
+    return 1
+  fi
+  NVM_DIR_PREV="$NVM_DIR"
   nvm unload
-  NPM_CONFIG_PREFIX=$_NPM_CONFIG_PREFIX
-  export NPM_CONFIG_PREFIX
+  export NPM_CONFIG_PREFIX="$_NPMCONFIGPREFOLD"
+  export NVM_DIR="$NVM_DIR_PREV"
 }
 
 
 # function to install NVM into $NVM_DIR
 function nvminstall {
+  # set NVM_DIR if not exist
+  if [[ -z ${NVM_DIR+x} ]]; then
+    export NVM_DIR="$DEF_NVM_DIR"
+  fi
+
   if [[ ! -d $NVM_DIR ]]; then
     git clone https://github.com/creationix/nvm.git "$NVM_DIR" &&
     cd "$NVM_DIR" &&
@@ -62,7 +70,7 @@ function nvminstall {
     echo "Error, $NVM_DIR is already exist, delete first before installation"
     return 1
   fi
-  echo "Error installing NVM"
+  echo "Error installing nvm"
   return 1
 }
 
@@ -73,12 +81,14 @@ function nvmupgrade {
     cd "$NVM_DIR"
     git fetch --tags origin &&
     git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` &&
-    \. "$NVM_DIR/nvm.sh"
+    
+    nvmload &&
+    
     return 0
   else
-    echo "Error, $NVM_DIR is NOT exist, install NVM first."
+    echo "Error, $NVM_DIR is NOT exist, install nvm first."
     return 1
   fi
-  echo "Error upgrading NVM"
+  echo "Error upgrading nvm"
   return 1
 }
